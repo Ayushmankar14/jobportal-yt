3a71cf5561a6da94d9ae17509db4bc7adc271d31
Ayushmankar14 <workpurpose908@gmail.com>
2025-07-19 22:36:14 +0530

    Update db.js

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
 
+const MONGO_URI = "mongodb+srv://workpurpose908:mr0m3VdkQkHMoXnR@cluster0.om9xa8k.mongodb.net/?retryWrites=true&w=majority";
+
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('mongodb connected successfully');
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("✅ MongoDB connected successfully");
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error.message);
+    process.exit(1); // Exit process with failure
+  }
+};
+
 export default connectDB;
